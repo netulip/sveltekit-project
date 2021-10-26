529d23e8d1bcd4912c4726d35c217246a57cca75
Mohamed Meftah <mohamed-ted@live.com>
2021-10-26 16:15:44 +0100

    cleanup

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,4 @@
 /// <reference types="@sveltejs/kit" />
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
 
 declare module '*&img'
 declare module 'virtual:windi-devtools'
--- windi.config.js
+++ windi.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	darkMode: false,
 	theme: {
 		extend: {}
